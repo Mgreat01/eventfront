b86412fdf716843eb27e111416cff7eae76f09d3
Sodja1234 <franklinkabisayi@gmail.com>
2025-05-19 19:51:16 +0100

    Destruction de la navbar et du footer au chargement de la route login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register/register.component';
+import { LoginComponent } from './login/login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+];
--- src/app/pages/navbar/navbar.component.ts
+++ src/app/pages/navbar/navbar.component.ts
@@ -1,30 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  isDarkTheme: boolean = false; 
-  isModalOpen: boolean = false; 
-  results: number = 100;
-  listResults = ['franklin', 'kabisayi', 'katambua'];
+  isDarkTheme: boolean = false;  
 
   toggleTheme(): void {
     this.isDarkTheme = ! this.isDarkTheme;
     const htmlPage = document.documentElement;
     htmlPage.classList.toggle('dark', this.isDarkTheme);
   }
-  openModal(): void {
-    this.isModalOpen = true;
-  }
-  closeModal(): void {
-    this.isModalOpen = false;
-  }
-  sendSearch():void {
-    this.closeModal();
-  }
+  
 }
