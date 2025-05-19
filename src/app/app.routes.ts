19dfa27fb22fb6cd5a09f95673b9fb11d11dbbba
Sodja1234 <franklinkabisayi@gmail.com>
2025-05-19 20:12:55 +0100

    Creation du composant Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register/register.component';
 import { LoginComponent } from './login/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
+    {path: '', component: HomeComponent},
 ];
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  imports: [ NgFor],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  images = [
+    'https://images.unsplash.com/photo-1626107096629-834f944251af?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGZyZWUlMjBhdWRpdG9yaXVtfGVufDB8fDB8fHww',
+    'https://images.unsplash.com/photo-1568772472528-9f7f5795e094?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGZyZWUlMjBhdWRpdG9yaXVtfGVufDB8fDB8fHww',
+    'https://plus.unsplash.com/premium_photo-1724753996329-3eef20c164c9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGZyZWUlMjBhdWRpdG9yaXVtfGVufDB8fDB8fHww',
+  ];
+
+  categories = [
+    'Musique', 'Société', 'Histoire', 'Littérature', 'Religion', 'Philosophie'
+  ];
+
+  currentIndex = 0;
+  
+  next(){
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prev(){
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  intervalId: any;
+
+  startAutoplay(){
+    this.intervalId = setInterval(() => { this.next()},3000)
+  }
+
+  ngOnInit(): void {
+    this.startAutoplay();
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId)
+  }
+}
