f7e9f344c7203f2100013e284f64b30f37dcccdf
Sodja1234 <franklinkabisayi@gmail.com>
2025-06-03 11:44:50 +0100

    Integration du service search dans la page Search et dans la search Card

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,10 +1,18 @@
-import { Component  } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
 import { SearchCardComponent } from '../search-card/search-card.component';
+import { SearchService } from '../../services/search.service';
+import { Category } from '../../models/category';
+import { EventSearch } from '../../models/event';
+import { Meta } from '../../models/meta';
+import { LinkUrl } from '../../models/link';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { from, firstValueFrom } from 'rxjs';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-search',
-  imports: [NgFor, SearchCardComponent, NgIf],
+  imports: [NgFor, SearchCardComponent, NgIf, ReactiveFormsModule, NgxPaginationModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
@@ -24,16 +32,82 @@ export class SearchComponent {
   ];
 
   isOpenDropDown: boolean = false;
+  searchService = inject(SearchService);
+  events: EventSearch[] = [];
+  pagination!: Meta;
+  pageUrl!: LinkUrl;
+  url!: string;
+  isLoaded: boolean = false;
+  prevPage!: string;
+  currentPage!: string;
+  nextPage!: string;
+  searchTerm: string = '';
+  private apiUrl = 'http://localhost:8000/api';
+  searchForm = new FormControl('');
+  perPage = new FormControl<number>(10);
+  errorCatch : boolean = false;
+  items = []; 
+
+  ngOnInit() {
+    this.loadEvents();
+  }
+
+  async loadEvents(url?: string) {
+    try {
+      const response = await firstValueFrom(this.searchService.getEvents(this.url));
+      this.isLoaded = !this.isLoaded;
+      this.events = response.data;
+      this.pagination = response.meta;
+      this.pageUrl = response.links;
+      this.isLoaded = true;
+      this.errorCatch = false;
+      console.log(this.pagination);
+
+      this.currentPage = `${this.pagination.path}&${this.pagination.current_page}`;
+      console.log(this.currentPage);
+      if (this.pageUrl.prev != null) {
+        this.prevPage = this.pageUrl.prev;
+      }
+      if (this.pageUrl.next != null) {
+        this.nextPage = this.pageUrl.next;
+      }
+    } catch (error: any) {
+      if (error.status === 404) {
+        console.error('Erreur 404 : Ressource non trouvée.');
+        console.log(error.status);
+        this.errorCatch = true;
+        this.events = [];
+
+      } else {
+        console.error('Une erreur est survenue :', error);
+      }
+      this.isLoaded = false;
+    }
+  }
+
+  goToPage(url: string) {
+    this.url = url;
+    this.ngOnInit();
+  }
+
+  submit(event: Event) {
+    event.preventDefault();
+    console.log(this.searchForm.value);
+    this.url = this.pagination.path + '?search=' + this.searchForm.value;
+    console.log('Terme de recherche :', this.url);
+
+    this.goToPage(this.url);
+  }
 
   toggleDropdown(): void {
     this.isOpenDropDown = !this.isOpenDropDown;
   }
 
   openDropdown(): void {
-    this.isOpenDropDown = false; 
+    this.isOpenDropDown = false;
   }
 
   resetFilters(): void {
-    
+
   }
 }
