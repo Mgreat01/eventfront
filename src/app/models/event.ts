8bd40c162b7631984fd7eeb28722676ef9b778fd
Sodja1234 <franklinkabisayi@gmail.com>
2025-06-03 11:40:25 +0100

    Creation des interfaces compatibles avec API de recherche

--- src/app/models/event.ts
+++ src/app/models/event.ts
@@ -1,15 +1,31 @@
+import { Category } from "./category";
+import { Ticket } from "./ticket";
+import { UserSearch } from "./user";
+
 export interface Event {
 
      basicInfo: {
     title: string;
-    category_ids: string[]; 
+    category_ids: string[];
   };
   details: {
     description: string;
   };
-  tickets: { 
-    ticketTypes: string[]; 
+  tickets: {
+    ticketTypes: string[];
     date_time_start: Date;
     date_time_end: Date;
   };
+}
+
+export  interface EventSearch {
+  id : number;
+  title: string;
+  description: string;
+  cycle: string;
+  categories : Category[];
+  created_by : UserSearch;
+  date_time_start: String;
+  date_time_end: String;
+  tickets: Ticket[];
 }
