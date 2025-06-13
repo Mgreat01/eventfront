541c1d9a4fe8629f6f62a3353d991cbe5fbeb27e
Mgreat01 <ephraimmonga5@gmail.com>
2025-06-13 12:54:30 +0200

    ajout de l affichage des tickets ( billets )  lié aux evenement de l utilisateur  connecté

--- src/app/pages/dashboard/composants/Billets/card-billet/card-billet.component.ts
+++ src/app/pages/dashboard/composants/Billets/card-billet/card-billet.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-billet',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './card-billet.component.html',
   styleUrl: './card-billet.component.css'
 })
 export class CardBilletComponent {
+  @Input() ticket: any;
 
+  getFormattedDateTime(date: string): string {
+    const d = new Date(date);
+    return d.toLocaleDateString('fr-FR', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric',
+    }) + ' | ' + d.toLocaleTimeString('fr-FR', {
+      hour: '2-digit',
+      minute: '2-digit'
+    });
+  }
+
+  getBadgeColor(): string {
+    return this.ticket.status === 'Confirmé' ? 'bg-green-100 text-green-800' : 'bg-yellow-100 text-yellow-800';
+  }
+
+
+
+  
 }
