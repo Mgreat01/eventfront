0bbb6d0e36dfd6ce6bfdc3e3f33d3a4522c20880
Mgreat01 <ephraimmonga5@gmail.com>
2025-06-13 12:56:14 +0200

    implementation des  methode de recuperation des eventements et tickets lié à l utilisateur connecté

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,10 +1,12 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { environment } from './../../environnement/environnement';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Category } from '../models/category';
 import { EventSearch } from '../models/event';
 import { ResponseThree } from '../models/response';
+import { UserService } from './user.service';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +15,7 @@ export class EventService {
 
   private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient , private userService: UserService) {}
 
   createEvent(data: any): Observable<any> {
     return this.http.post(`${this.apiUrl}/evenements`, data);
@@ -23,6 +25,7 @@ export class EventService {
     return this.http.get<Category[]>(`${this.apiUrl}/categories`);
   }
 
+
   getEventById(id: number): Observable<{ data: EventSearch }> {
     return this.http.get<{ data: EventSearch }>(`${this.apiUrl}/event/id/${id}`);
   }
@@ -30,4 +33,37 @@ export class EventService {
   getLastEvents(): Observable<ResponseThree<EventSearch>> {
     return this.http.get<ResponseThree<EventSearch>>(`${this.apiUrl}/event/three`);
   }
+
+
+   getOrganizerEvents(filter: string = 'all', search: string = ''): Observable<any> {
+   // const token = this.userService.getToken();
+   const token = "1|ixjHKHqQXEQ0MiNmpKwnJBUXQdg8gzswzMyi6AJN66498661"
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    let params = new HttpParams();
+    if (search) {
+      params = params.set('search', search);
+    }
+
+    return this.http.get(`${this.apiUrl}/dashboard/organizer/events/${filter}`, {
+      headers,
+      params,
+    });
+  }
+
+  getOrganizerTickets(): Observable<any> {
+  const token = this.userService.getToken() || "1|ixjHKHqQXEQ0MiNmpKwnJBUXQdg8gzswzMyi6AJN66498661"; // fallback si tu fais des tests
+
+  const headers = new HttpHeaders({
+    Authorization: `Bearer ${token}`,
+  });
+
+  return this.http.get(`${this.apiUrl}/dashboard/user/tickets`, {
+    headers,
+  });
+}
+
 }
