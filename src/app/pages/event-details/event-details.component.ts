46c384a07f8ffe638304bb71fae037bbb48b2741
Sodja1234 <franklinkabisayi@gmail.com>
2025-06-16 16:16:03 +0100

    Recuperation dynamique des derniers Events

--- src/app/pages/event-details/event-details.component.ts
+++ src/app/pages/event-details/event-details.component.ts
@@ -4,32 +4,45 @@ import { EventService } from '../../services/event.service';
 import { firstValueFrom } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { EventCardComponent } from '../event-card/event-card.component';
+import { QuillModule } from 'ngx-quill';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-event-details',
-  imports: [EventCardComponent],
+  standalone: true,
+  imports: [EventCardComponent, QuillModule, NgFor],
   templateUrl: './event-details.component.html',
   styleUrl: './event-details.component.css'
 })
-export class EventDetailsComponent implements OnInit {
+export class EventDetailsComponent{
   event: EventSearch = {} as EventSearch;
+  eventThrees: EventSearch[] = [];
   eventService: EventService = inject(EventService);
-  id: number = 0;
-  constructor(private route: ActivatedRoute) { }
+  id: number = -1;
+  descriptionHtml: SafeHtml | undefined;
+
+  constructor(private route: ActivatedRoute, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.id = parseInt(params['id']);
+      this.loadEvent(this.id);
     });
     console.log(this.id);
-    this.loadEvent(this.id);
   }
 
   async loadEvent(id: number) {
     try {
       const response = await firstValueFrom(this.eventService.getEventById(id));
+      const responseLastEvent = await firstValueFrom(this.eventService.getLastEvents(3, id));
+
       this.event = response.data;
+      this.eventThrees = responseLastEvent.data;
+
+      this.descriptionHtml = this.sanitizer.bypassSecurityTrustHtml(this.event.description);
       console.log(this.event);
+      console.log(this.eventThrees);
 
     } catch (error: any) {
       if (error.status === 404) {
